f76457e82f4609beeb508c40e8809c9e6880715c
j1sk1ss <fotikolay222@gmail.com>
2026-06-11 02:03:39 -0700

    [IDT] Have rewritten the IDT module on CPL

--- src/kernel/include/arch/i386/tss.h
+++ src/kernel/include/arch/i386/tss.h
@@ -1,9 +1,8 @@
-#ifndef TSS_H
-#define TSS_H
+#ifndef TSS_H_
+#define TSS_H_
 
 #include <stdint.h>
 
-extern void TSS_init(uint32_t idx, uint32_t kss, uint32_t kesp);
 extern void TSS_set_stack(uint32_t kss, uint32_t kesp);
 
 #endif
